2cd29cd12fee3a8a3263dd6a7f8abf4465d83735
KarimOS <166505318+karim-chatty@users.noreply.github.com>
2025-05-22 19:14:07 +0100

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime ,timedelta
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+#   U CAN USE THE LINK WHERE TO SEND REQUESTS TO  IN A SEPARETE CONSTANTE AS U SEE API& BASE URL ,OR JUST PUT THE FULL LINK,FIRST ONE IS BETTER 
+# API_KEY = "2bddd46deb88b2726c2681492bc44363"
+# BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+name=input("What is your Name :")
+ur_country= input("Enter your country| city:")
+ # THIS lines to get the targeted day 
+dayswanted=int(input("enter the Day u want (ex:1=>tomorrow,2=>after Tomorrow,3=>thirdday ...etc) :"))
+futerdate=datetime.now()+ timedelta(days=dayswanted)
+print(futerdate)
+
+API_KEY= os.getenv("API_KEY")
+
+complete_url = f"http://api.openweathermap.org/data/2.5/forecast?q={ur_country}&appid={API_KEY}"
+response = requests.get(complete_url)
+whatweget = response.json()
+
+print(response.status_code)  #THIS LINE IS TO RETURN IF OUR DATA ACCESSED OR no BY RETURNING THE status code 
+
+for forecast in whatweget['list']:  
+    
+ print(f"Hello {name}, The Weather in {ur_country} on {futerdate}: \nTemperature: {forecast['main']['temp']-273.15} C \n wind speed:{forecast['wind']['speed']}\nHumidity:{forecast['main']['humidity']}")
+
+
+
